018e2aa6768705c4dc8c643a35c5d9e6f07a5d3d
Isha-121 <77353197+Isha-121@users.noreply.github.com>
2021-08-09 15:29:29 +0530

    updated src file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Restaurant from './component/Basics/Restaurant';
-import UseState from "./component/Hooks/UseState";
-import UseEffect from './component/Hooks/UseEffect';
-import UseReducer from './component/Hooks/UseReducer';
-import ToDo from './component/ToDo React/ToDo';
-import Temp from './component/Weather/temp';
+
 const App = () => {
   return (
     <>
